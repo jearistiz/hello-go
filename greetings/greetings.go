0d8aa4532b58200940bde2cb4cbadf5840a57332
Juan Esteban Aristizabal <jeaz.git@gmail.com>
2022-05-01 14:02:14 -0500

    feat: error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	// The above loc is equivalent to the following two commented loc
+
+	message := fmt.Sprintf("Hi, %v. Welcome to ON THE GO!", name)
+
+	// The above LOC is equivalent to the following two commented LsOC
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
+
 	return message
+
+}
+
+func HelloWithError(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	return Hello(name), nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"rsc.io/quote"
 
@@ -9,9 +12,48 @@ import (
 )
 
 func main() {
+	simpleMain()
+	errorHandlingMain()
+}
+
+func simpleMain() {
+	fmt.Println("\n--- Start simpleMain exec ---")
+
 	message := greetings.Hello("Jose David")
+	fmt.Println(message)
+	fmt.Println(quote.Go())
+
+	fmt.Println("--- End simpleMain exec ---")
+}
+
+func errorHandlingMain() {
+
+	fmt.Println("\n--- Start errorHandlingMain exec ---")
+	// Sets the Logger prefix
+	log.SetPrefix("greetings: ")
+	// Avoids printing time, source file and line number in logs
+	// log.SetFlags(0)
+
+	// Scan name from terminal
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Type your name, then press Enter: ")
+	fullName, err := reader.ReadString('\n')
+
+	// Handling error from ReadString function
+	if err != nil {
+		msg := fmt.Sprintf("\n--- End errorHandlingMain exec with errors: %v---", err)
+		log.Fatal(msg)
+	}
+
+	// Call HelloWithError funcition using user input name
+	message, err := greetings.HelloWithError(fullName[:len(fullName)-1])
+	// Handling error from HelloWithError function
+	if err != nil {
+		msg := fmt.Sprintf("\n--- End errorHandlingMain exec with errors: %v---", err)
+		log.Fatal(msg)
+	}
 
 	fmt.Println(message)
 
-	fmt.Println(quote.Go())
+	fmt.Println("--- End errorHandlingMain exec ---")
 }
